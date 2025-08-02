dc3f08cf952b98e98f583008b3d31f7a5f21d160
Sang Le <3lesang@gmail.com>
2025-08-02 14:39:43 +0700

    update build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,41 +1,22 @@
 // @ts-check
 import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
-import tailwindcss from "@tailwindcss/vite";
-import AstroPWA from "@vite-pwa/astro";
-import { defineConfig } from "astro/config";
-
 import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  devToolbar: {
-    enabled: false,
-  },
   build: {
     format: "file",
   },
-
   vite: {
     plugins: [tailwindcss()],
   },
-
-  integrations: [react(), AstroPWA(), sitemap()],
-
+  integrations: [react()],
   prefetch: {
     defaultStrategy: "hover",
     prefetchAll: true,
   },
-
   trailingSlash: "never",
-
-  i18n: {
-    locales: ["en", "vi"],
-    defaultLocale: "en",
-    routing: {
-      prefixDefaultLocale: false,
-    },
-  },
-
   adapter: vercel(),
 });
