5d76833cccc4cf3e7817e975da593b80f9c4384a
Sang Le <3lesang@gmail.com>
2025-08-08 10:27:17 +0700

    feat(cart): change to alpine

--- src/entrypoint.ts
+++ src/entrypoint.ts
@@ -1,7 +1,8 @@
 import focus from "@alpinejs/focus";
 import intersect from "@alpinejs/intersect";
+import persist from "@alpinejs/persist";
 import type { Alpine } from "alpinejs";
 
 export default (Alpine: Alpine) => {
-  Alpine.plugin([intersect, focus]);
+  Alpine.plugin([intersect, focus, persist]);
 };
