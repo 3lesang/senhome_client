e0148ee68d6dbdbafd2805115da82acb27307077
Sang Le <3lesang@gmail.com>
2025-08-11 15:48:32 +0700

    update alpine

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
+import alpinejs from "@astrojs/alpinejs";
 import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-
-import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
